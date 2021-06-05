8c70364cab79e1f9a350daad8fa919b11943bf2b
Ehsan Motahari <e.motahari@gmail.com>
2021-06-05 22:39:55 +0430

    4th

--- budgeting/views.py
+++ budgeting/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+
 def income_view(request):
-    pass
+    return HttpResponseRedirect(reverse('budgeting:income'))

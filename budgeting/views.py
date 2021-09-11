9b4e746ebcae898b25bfd03b4be1e6ec1c2b60bb
Ehsan Motahari <e.motahari@gmail.com>
2021-09-11 13:44:01 +0430

    3

--- budgeting/views.py
+++ budgeting/views.py
@@ -426,23 +426,297 @@ def addLoanCost(request, id, loanCostId=0):
 # ########################  END  #######################################
 
 
+def calculateIncome(data):
+
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realIncomeQ1
+        x2 = x2 + c.realIncomeQ2
+        x3 = x3 + c.realIncomeQ3
+        x4 = x4 + c.realIncomeQ4
+        y1 = y1 + c.forcastIncomeQ1
+        y2 = y2 + c.forcastIncomeQ2
+        y3 = y3 + c.forcastIncomeQ3
+        y4 = y4 + c.forcastIncomeQ4
+
+    incomeData = {'realIncomeQ1': x1,
+                  'realIncomeQ2': x1 + x2,
+                  'realIncomeQ3': x1 + x2 + x3,
+                  'realIncomeQ4': x1 + x2 + x3 + x4,
+                  'focastIncomeQ1': y1,
+                  'focastIncomeQ2': y1 + y2,
+                  'focastIncomeQ3': y1 + y2 + y3,
+                  'focastIncomeQ4': y1 + y2 + y3 + y4
+                  }
+
+    return incomeData
+
+def calculateCostOfSale(data):
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realCostQ1
+        x2 = x2 + c.realCostQ2
+        x3 = x3 + c.realCostQ3
+        x4 = x4 + c.realCostQ4
+        y1 = y1 + c.forcastCostQ1
+        y2 = y2 + c.forcastCostQ2
+        y3 = y3 + c.forcastCostQ3
+        y4 = y4 + c.forcastCostQ4
+
+    costOfSaleData = {'realCostQ1': x1,
+                  'realCostQ2': x1 + x2,
+                  'realCostQ3': x1 + x2 + x3,
+                  'realCostQ4': x1 + x2 + x3 + x4,
+                  'forcastCostQ1': y1,
+                  'forcastCostQ2': y1 + y2,
+                  'forcastCostQ3': y1 + y2 + y3,
+                  'forcastCostQ4': y1 + y2 + y3 + y4
+                  }
+
+    return costOfSaleData
+
+
+def calculateEtcOprIcome(data):
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realetcOprationIncomeQ1
+        x2 = x2 + c.realetcOprationIncomeQ2
+        x3 = x3 + c.realetcOprationIncomeQ3
+        x4 = x4 + c.realetcOprationIncomeQ4
+        y1 = y1 + c.forcastetcOprationIncomeQ1
+        y2 = y2 + c.forcastetcOprationIncomeQ2
+        y3 = y3 + c.forcastetcOprationIncomeQ3
+        y4 = y4 + c.forcastetcOprationIncomeQ4
+
+    etcOprIcomeData = {'realetcOprationIncomeQ1': x1,
+                       'realetcOprationIncomeQ2': x1 + x2,
+                       'realetcOprationIncomeQ3': x1 + x2 + x3,
+                       'realetcOprationIncomeQ4': x1 + x2 + x3 + x4,
+                       'forcastetcOprationIncomeQ1': y1,
+                       'forcastetcOprationIncomeQ2': y1 + y2,
+                       'forcastetcOprationIncomeQ3': y1 + y2 + y3,
+                       'forcastetcOprationIncomeQ4': y1 + y2 + y3 + y4
+                      }
+
+    return etcOprIcomeData
+
+def calculateEtcOprCost(data):
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realEtcOprationCostQ1
+        x2 = x2 + c.realEtcOprationCostQ2
+        x3 = x3 + c.realEtcOprationCostQ3
+        x4 = x4 + c.realEtcOprationCostQ4
+        y1 = y1 + c.forcastEtcOprationCostQ1
+        y2 = y2 + c.forcastEtcOprationCostQ2
+        y3 = y3 + c.forcastEtcOprationCostQ3
+        y4 = y4 + c.forcastEtcOprationCostQ4
+
+    etcOprCostData = {'realEtcOprationCostQ1': x1,
+                      'realEtcOprationCostQ2': x1 + x2,
+                      'realEtcOprationCostQ3': x1 + x2 + x3,
+                      'realEtcOprationCostQ4': x1 + x2 + x3 + x4,
+                      'forcastEtcOprationCostQ1': y1,
+                      'forcastEtcOprationCostQ2': y1 + y2,
+                      'forcastEtcOprationCostQ3': y1 + y2 + y3,
+                      'forcastEtcOprationCostQ4': y1 + y2 + y3 + y4
+                      }
+
+    return etcOprCostData
+
+
+def calculateNonOprIncome(data):
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realNonOprationIncomeQ1
+        x2 = x2 + c.realNonOprationIncomeQ2
+        x3 = x3 + c.realNonOprationIncomeQ3
+        x4 = x4 + c.realNonOprationIncomeQ4
+        y1 = y1 + c.forcastNonOprationIncomeQ1
+        y2 = y2 + c.forcastNonOprationIncomeQ2
+        y3 = y3 + c.forcastNonOprationIncomeQ3
+        y4 = y4 + c.forcastNonOprationIncomeQ4
+
+    nonOprIncomeData = {'realNonOprationIncomeQ1': x1,
+                        'realNonOprationIncomeQ2': x1 + x2,
+                        'realNonOprationIncomeQ3': x1 + x2 + x3,
+                        'realNonOprationIncomeQ4': x1 + x2 + x3 + x4,
+                        'forcastNonOprationIncomeQ1': y1,
+                        'forcastNonOprationIncomeQ2': y1 + y2,
+                        'forcastNonOprationIncomeQ3': y1 + y2 + y3,
+                        'forcastNonOprationIncomeQ4': y1 + y2 + y3 + y4
+                      }
+
+    return nonOprIncomeData
+
+
+def calculateNonOprCost(data):
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realNonOprationCostQ1
+        x2 = x2 + c.realNonOprationCostQ2
+        x3 = x3 + c.realNonOprationCostQ3
+        x4 = x4 + c.realNonOprationCostQ4
+        y1 = y1 + c.forcastNonOprationCostQ1
+        y2 = y2 + c.forcastNonOprationCostQ2
+        y3 = y3 + c.forcastNonOprationCostQ3
+        y4 = y4 + c.forcastNonOprationCostQ4
+
+    nonOprCostData = {'realNonOprationCostQ1': x1,
+                        'realNonOprationCostQ2': x1 + x2,
+                        'realNonOprationCostQ3': x1 + x2 + x3,
+                        'realNonOprationCostQ4': x1 + x2 + x3 + x4,
+                        'forcastNonOprationCostQ1': y1,
+                        'forcastNonOprationCostQ2': y1 + y2,
+                        'forcastNonOprationCostQ3': y1 + y2 + y3,
+                        'forcastNonOprationCostQ4': y1 + y2 + y3 + y4
+                      }
+
+    return nonOprCostData
+
+def calculateTax(data):
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realTaxQ1
+        x2 = x2 + c.realTaxQ2
+        x3 = x3 + c.realTaxQ3
+        x4 = x4 + c.realTaxQ4
+        y1 = y1 + c.forcastTaxQ1
+        y2 = y2 + c.forcastTaxQ2
+        y3 = y3 + c.forcastTaxQ3
+        y4 = y4 + c.forcastTaxQ4
+
+    taxData = {'realTaxQ1': x1,
+               'realTaxQ2': x1 + x2,
+               'realTaxQ3': x1 + x2 + x3,
+               'realTaxQ4': x1 + x2 + x3 + x4,
+               'forcastTaxQ1': y1,
+               'forcastTaxQ2': y1 + y2,
+               'forcastTaxQ3': y1 + y2 + y3,
+               'forcastTaxQ4': y1 + y2 + y3 + y4
+               }
+
+    return taxData
+
+def calculateLoanCost(data):
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
+    y1 = 0
+    y2 = 0
+    y3 = 0
+    y4 = 0
+
+    for c in data:
+        x1 = x1 + c.realLoanCostQ1
+        x2 = x2 + c.realLoanCostQ2
+        x3 = x3 + c.realLoanCostQ3
+        x4 = x4 + c.realLoanCostQ4
+        y1 = y1 + c.forcastLoanCostQ1
+        y2 = y2 + c.forcastLoanCostQ2
+        y3 = y3 + c.forcastLoanCostQ3
+        y4 = y4 + c.forcastLoanCostQ4
+
+    loanCostData = {'realLoanCostQ1': x1,
+               'realLoanCostQ2': x1 + x2,
+               'realLoanCostQ3': x1 + x2 + x3,
+               'realLoanCostQ4': x1 + x2 + x3 + x4,
+               'forcastLoanCostQ1': y1,
+               'forcastLoanCostQ2': y1 + y2,
+               'forcastLoanCostQ3': y1 + y2 + y3,
+               'forcastLoanCostQ4': y1 + y2 + y3 + y4
+               }
+
+    return loanCostData
+
+
+
 @login_required
 def benefitSheetCal(request, id, year):
-    income = get_object_or_404(Income,pk=id,yearOfForcast=year )
-    sumOfrealIncomeQ1 = 0
-    sumOfrealIncomeQ2 = 0
-    sumOfrealIncomeQ3 = 0
-    sumOfrealIncomeQ4 = 0
-    for c in income:
-        sumOfrealIncomeQ1 = sumOfrealIncomeQ1 + c.realIncomeQ1
-        sumOfrealIncomeQ2 = sumOfrealIncomeQ2 + c.realIncomeQ2
-        sumOfrealIncomeQ3 = sumOfrealIncomeQ3 + c.realIncomeQ3
-        sumOfrealIncomeQ4 = sumOfrealIncomeQ4 + c.realIncomeQ4
-        sumOfforcastIncomeQ1 = sumOfforcastIncomeQ1 + c.forcastIncomeQ1
-        sumOfforcastIncomeQ2 = sumOfforcastIncomeQ2 + c.forcastIncomeQ2
-        sumOfforcastIncomeQ3 = sumOfforcastIncomeQ3 + c.forcastIncomeQ3
-        sumOfforcastIncomeQ4 = sumOfforcastIncomeQ4 + c.forcastIncomeQ4
+    company = get_object_or_404(Company, pk=id)
+    income = Income.objects.filter(company=id, yearOfForcast=year)
+    costOfSales = CostOfSales.objects.filter(company=id, yearOfForcast=year)
+    etcOprIcome = EtcOprationalIncome.objects.filter(company=id, yearOfForcast=year)
+    etcOprCost = EtcOprationalCost.objects.filter(company=id, yearOfForcast=year)
+    nonOprIncome = NonOprationalIncome.objects.filter(company=id, yearOfForcast=year)
+    nonOprCost = NonOprationalCost.objects.filter(company=id, yearOfForcast=year)
+    tax = Tax.objects.filter(company=id, yearOfForcast=year)
+    loanCost = LoanCost.objects.filter(company=id, yearOfForcast=year)
+    incomeData = calculateIncome(income)
+    costOfSalesData = calculateCostOfSale(costOfSales)
+    etcOprIcomeData = calculateEtcOprIcome(etcOprIcome)
+    etcOprCostData = calculateEtcOprCost(etcOprCost)
+    nonOprIncomeData = calculateNonOprIncome(nonOprIncome)
+    nonOprCostData = calculateNonOprCost(nonOprCost)
+    taxData = calculateTax(tax)
+    loanCostData = calculateLoanCost(loanCost)
+    etcOfSheet = {'sood11': incomeData['realIncomeQ1'] - costOfSalesData['realCostQ1']}
 
-    incomeData = {'income1' : sumOfrealIncomeQ1, 'incom2': sumOfrealIncomeQ1 + sumOfrealIncomeQ2}
-    return  render(request, 'budgeting/sheet.html', {'incomdata':incomeData })
+    return  render(request, 'budgeting/sheet.html', {'incomdata': incomeData,
+                                                     'costOfSalesData': costOfSalesData,
+                                                     'etcOprIcomeData' : etcOprIcomeData,
+                                                     'etcOprCostData' : etcOprCostData,
+                                                     'nonOprIncomeData' : nonOprIncomeData,
+                                                     'nonOprCostData' : nonOprCostData,
+                                                     'taxData' : taxData,
+                                                     'loanCostData' : loanCostData,
+                                                     'etcOfSheet' : etcOfSheet,
+                                                     'companies': company })
 
